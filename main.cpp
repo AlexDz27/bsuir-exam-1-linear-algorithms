bd08e139a4bc1764773b97d05d64e4df5d83d3d6
Alex Dzyuba <alexeydzyuba27@gmail.com>
2021-10-16 14:58:54 +0300

    Set up main development code

--- main.cpp
+++ main.cpp
@@ -5,20 +5,11 @@ using namespace std;
 int main() {
   // x = 0.03981, y = -1625, z = 0.512 -> a = 1.2619
 
-  double x, y, z;
-  double result;
+  double x = 0.03981;
+  double y = -1625;
+  double z = 0.512;
 
-  cout << "Enter values for variables 'x', 'y', 'z'" << endl;
-  cout << "X: "; cin >> x;
-  cout << "Y: "; cin >> y;
-  cout << "Z: "; cin >> z;
 
-  if (!cin.good()) { // If user entered values other than numbers
-    cout << "Error: you've entered bad values into one of the variables. All variables must be numbers. Exiting the program...";
-    return 1;
-  }
-
-  cout << "Going further" << endl;
 
   return 0;
 }
